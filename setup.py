606713515a9dec8e7d641a516a35d85be3afb6f0
Paul Russell <russe1pt@gmail.com>
2018-11-13 20:21:25 -0500

    chore: package manager configs

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='python_eda',
+      version='0.01',
+      description='Python Exploratory Data Analysis',
+      packages=['python_eda'],
+      author='Paul Russell',
+      author_email='russe1pt@gmail.com',
+      zip_safe=False)
